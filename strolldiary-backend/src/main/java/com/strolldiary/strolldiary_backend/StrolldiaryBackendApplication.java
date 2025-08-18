88d5f9cceb9ed142f1e7f5e0e975e84da548cd6c
BryOliveira <jerryrules2004slime@gmail.com>
2025-08-17 20:57:32 -0400

    Skeleton of Strolldiary

---
+++ strolldiary-backend/src/main/java/com/strolldiary/strolldiary_backend/StrolldiaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.strolldiary.strolldiary_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrolldiaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrolldiaryBackendApplication.class, args);
+	}
+
+}
---
+++ strolldiary-backend/src/test/java/com/strolldiary/strolldiary_backend/StrolldiaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strolldiary.strolldiary_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrolldiaryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
